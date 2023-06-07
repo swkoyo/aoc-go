b0165d7a6f93fedf86d93f99e4e7522ff0f71eef
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-06-07 12:52:47 +0900

    2022d4

---
+++ 2022/d4/p1.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+    "strconv"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatalln("Error reading file", err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln("Error scanning file", err)
+	}
+
+	total := 0
+
+	for scanner.Scan() {
+        assignments := strings.Split(scanner.Text(), ",")
+        assignment1 := strings.Split(assignments[0], "-")
+        assignment2 := strings.Split(assignments[1], "-")
+
+        assignment1Start, err := strconv.Atoi(assignment1[0]);
+        if err != nil {
+            log.Fatalln("Error converting to int", err)
+        }
+        assignment1End, err := strconv.Atoi(assignment1[1]);
+        if err != nil {
+            log.Fatalln("Error converting to int", err)
+        }
+        assignment2Start, err := strconv.Atoi(assignment2[0]);
+        if err != nil {
+            log.Fatalln("Error converting to int", err)
+        }
+        assignment2End, err := strconv.Atoi(assignment2[1]);
+        if err != nil {
+            log.Fatalln("Error converting to int", err)
+        }
+
+        if ((assignment1Start <= assignment2Start && assignment1End >= assignment2End) || (assignment2Start <= assignment1Start && assignment2End >= assignment1End)) {
+            total++
+        }
+	}
+
+	fmt.Println(total)
+}
